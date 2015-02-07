ec1ed513dba64e479911eb86bde303f031a30855
Michael Zamani <michael.zamani1@gmail.com>
2015-02-07 12:28:10 -0500

    Initial commit for the Decision Tree classifier

---
+++ twitchNLP/NLP/BuildAndTrainTwitchClassifier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+from __future__ import division
+import nltk, pickle, random
+from nltk.corpus import PlaintextCorpusReader
+
+def twitch_chat_features(document):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__": main()
